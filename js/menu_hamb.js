ff4359ef67060c5bdafe746f180c8aaed3f2cf71
Samuel Vinícius <samuelvinib@gmail.com>
2022-05-22 23:17:29 -0300

    versão melhorada

--- js/menu_hamb.js
+++ js/menu_hamb.js
@@ -1,13 +1,14 @@
 const menu_hamb = document.querySelector("[data-hamb]");
+var imgHamb = document.querySelector("[data-close]")
 const menu1 = document.querySelector("[data-menu]");
 menu_hamb.addEventListener('click', randonclick);
 function randonclick(){
     menu_hamb.classList.toggle("active");
     menu1.classList.toggle("active");
     if(menu_hamb.classList.contains('active')){
-        menu_hamb.innerHTML =  '<span class="head__hamb" data-hamb=""><img data-close="" src="img//icons/close.svg" alt="menu hamburguer" title="menu"></span>';
+        imgHamb.src = 'img/icons/close.svg' ;
     }
     else{
-        menu_hamb.innerHTML = '<span class="head__hamb" data-hamb=""><img data-close="" src="img/icons/menu.svg" alt="menu hamburguer" title="menu"></span>';
+        imgHamb.src = 'img/icons/menu.svg';
     }
 }//menu hamburguer
